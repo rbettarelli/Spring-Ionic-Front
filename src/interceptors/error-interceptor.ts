b5a64b39ce415edf5c077fb559ecee08eab8120e
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-01-26 18:50:11 -0300

    Savando um novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -4,45 +4,65 @@ import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { isRightSide } from 'ionic-angular/umd/util/util';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
     constructor(public storage: StorageService, public alertCtrl: AlertController) {
-        
+
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        
+
         return next.handle(req)
-        .catch((error, caught) => {
+            .catch((error, caught) => {
 
-            let errorObj = error;
-            if (errorObj.error) {
-                errorObj = errorObj.error;
-            }
-            if (!errorObj.status) {
-                errorObj = JSON.parse(errorObj);
-            }
+                let errorObj = error;
+                if (errorObj.error) {
+                    errorObj = errorObj.error;
+                }
+                if (!errorObj.status) {
+                    errorObj = JSON.parse(errorObj);
+                }
 
-            console.log("Erro detectado pelo interceptor:");
-            console.log(errorObj);
+                console.log("Erro detectado pelo interceptor:");
+                console.log(errorObj);
 
-            switch(errorObj.status) {
-                case 401:
-                    this.handle401();
-                    break;
-                case 403:
-                    this.handle403();
-                    break;
+                switch (errorObj.status) {
+                    case 401:
+                        this.handle401();
+                        break;
+                    case 403:
+                        this.handle403();
+                        break;
+
+                    case 422:
+                        this.handle422(errorObj);
+                        break;
 
                     default:
                         this.handleDefaultError(errorObj)
 
-            }
+                }
 
-            return Observable.throw(error);
-        }) as any;
+                return Observable.throw(error);
+            }) as any;
+    }
+
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+
     }
 
     handle403() {
@@ -61,12 +81,23 @@ export class ErrorInterceptor implements HttpInterceptor {
             buttons: [
                 {
                     text: 'Ok'
-                    }
+                }
             ]
         });
         alert.present();
-        
+
     }
+
+    private listErrors(messages: FieldMessage[]): string {
+        let s: string = '';
+        for (var i = 0; i < messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>:  " + messages[i].message + '</p>'
+        }
+
+        return s;
+    }
+
+
 
     handleDefaultError(errorObj) {
 
@@ -77,15 +108,17 @@ export class ErrorInterceptor implements HttpInterceptor {
             buttons: [
                 {
                     text: 'Ok'
-                    }
+                }
             ]
         });
         alert.present();
-        
+
 
     }
 
 }
+
+
 
 export const ErrorInterceptorProvider = {
     provide: HTTP_INTERCEPTORS,
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CidadeDTO } from '../../models/cidade.dto';
 import { EstadoDTO } from '../../models/estado.dto';
 import { CidadeService } from '../../services/domain/cidade.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 import { EstadoService } from '../../services/domain/estado.service';
 
 
@@ -19,32 +20,34 @@ export class SignupPage {
   estados: EstadoDTO[];
   cidades: CidadeDTO[];
 
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public formBuilder: FormBuilder,
-    public cidadeService : CidadeService,
-    public estadoService: EstadoService) {
+    public cidadeService: CidadeService,
+    public estadoService: EstadoService,
+    public clienteService: ClienteService,
+    public alertCtrl: AlertController) {
 
-      this.formGroup = this.formBuilder.group({
-        nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
-        email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
-        tipo : ['1', [Validators.required]],
-        cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
-        senha : ['123', [Validators.required]],
-        logradouro : ['Rua Via', [Validators.required]],
-        numero : ['25', [Validators.required]],
-        complemento : ['Apto 3', []],
-        bairro : ['Copacabana', []],
-        cep : ['10828333', [Validators.required]],
-        telefone1 : ['977261827', [Validators.required]],
-        telefone2 : ['', []],
-        telefone3 : ['', []],
-        estadoId : [null, [Validators.required]],
-        cidadeId : [null, [Validators.required]]      
+    this.formGroup = this.formBuilder.group({
+      nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
+      tipo: ['1', [Validators.required]],
+      cpfOuCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha: ['123', [Validators.required]],
+      logradouro: ['Rua Via', [Validators.required]],
+      numero: ['25', [Validators.required]],
+      complemento: ['Apto 3', []],
+      bairro: ['Copacabana', []],
+      cep: ['10828333', [Validators.required]],
+      telefone1: ['977261827', [Validators.required]],
+      telefone2: ['', []],
+      telefone3: ['', []],
+      estadoId: [null, [Validators.required]],
+      cidadeId: [null, [Validators.required]]
 
 
-      }) 
-      
+    })
+
   }
 
   ionViewDidLoad() {
@@ -54,7 +57,7 @@ export class SignupPage {
         this.formGroup.controls.estadoId.setValue(this.estados[0].id);
         this.updateCidades();
       },
-      error => {});
+        error => { });
   }
 
   updateCidades() {
@@ -64,15 +67,39 @@ export class SignupPage {
         this.cidades = response;
         this.formGroup.controls.cidadeId.setValue(null);
       },
-      error => {});
+        error => { });
   }
 
-  
+
 
   signupUser() {
-    console.log("enviou o form")
+
+    this.clienteService.insert(this.formGroup.value)
+      .subscribe(response => {
+        this.showInsertOk();
+
+      }, error => { });
 
 
+  }
+
+  showInsertOk() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso',
+      message: 'Cadastro realizado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+
+        }
+      ]
+
+    });
+    alert.present();
   }
 
 
