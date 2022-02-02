e5d32e64ebb74d0b0bdcdd657b95d9f5da24f73f
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-02-02 19:56:19 -0300

    Carregando dados do Produto

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
 
 
 @IonicPage()
@@ -13,19 +15,35 @@ export class ProdutoDetailPage {
   item: ProdutoDTO;
 
   constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams) {
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtosService: ProdutoService) {
   }
 
   ionViewDidLoad() {
 
-    this.item = {
-      id: "1",
-      nome: "Mouse",
-      preco: 80.59 
+    let produto_id = this.navParams.get('produto_id');
 
-    }
-   
+    this.produtosService.findById(produto_id)
+      .subscribe(response => {
+        this.item = response;
+        this.getImageUrlIfExists();
+
+      },
+        error => { });
+
+
+
+
+  }
+
+  getImageUrlIfExists() {
+    this.produtosService.getImageFromBucket(this.item.id)
+    .subscribe(response => {
+      this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`
+
+    },
+    error => { });
   }
 
 
