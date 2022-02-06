05cd1e66e8220736335f11f0c967894e79e70e7a
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-02-06 09:43:18 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,8 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { CartService } from '../../services/domain/cart.service';
+import { PedidoService } from '../../services/domain/pedido.service';
+import { Response } from '@angular/http';
 
 @IonicPage()
 @Component({
@@ -23,7 +25,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public clienteService: ClienteService,
-    public cartService: CartService) {
+    public cartService: CartService,
+    public pedidoService: PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
   }
@@ -49,4 +52,27 @@ export class OrderConfirmationPage {
   total() : number {
     return this.cartService.total();
   } 
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
+
+
+  checkOut() {
+
+    this.pedidoService.insert(this.pedido)
+    .subscribe(response => {
+      this.cartService.createOrClearCart();
+      console.log(response.headers.get('location'));
+
+    },
+    error => {
+      if (error.status == 403 ) {
+        this.navCtrl.setRoot('HomePage')
+      }
+
+    });
+
+  }
+
 }
