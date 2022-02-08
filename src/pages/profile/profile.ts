d0ae0f7815ae4e8710afa7ececce57c96f8f7cc6
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-02-08 12:30:07 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,6 +26,11 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+    
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -68,5 +73,18 @@ export class ProfilePage {
      this.cameraOn = false;
     }, (err) => {
     });
+  }
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(response => {
+      this.picture = null;
+      this.loadData();
+    },
+    error => {
+
+    });
+  }
+  cancel() {
+    this.picture = null;
   }
 }
